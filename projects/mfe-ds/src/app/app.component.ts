720f6bfebea49ea8d38b9caeb72216dd8b335193
Wiliane Souza <wiliane_souza@hotmail.com>
2025-02-21 19:14:54 -0300

    feat(mfe-ds): add control to field

--- projects/mfe-ds/src/app/app.component.ts
+++ projects/mfe-ds/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { FieldComponent } from "./components/field/field.component";
-import { ButtonComponent } from './components/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FieldComponent, ButtonComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- projects/mfe-ds/src/app/components/field/field.component.ts
+++ projects/mfe-ds/src/app/components/field/field.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 export type InputType = 'text' | 'number' | 'password';
 
 @Component({
   selector: 'app-field',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './field.component.html',
   styleUrl: './field.component.scss',
 })
 export class FieldComponent {
   @Input() type: InputType = 'text';
   @Input() placeholder?: string;
+  @Input() control: FormControl = new FormControl();
 }
