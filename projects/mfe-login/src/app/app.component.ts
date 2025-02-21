7725726d1be7994d579991ea12e998f3715c99ad
Wiliane Souza <wiliane_souza@hotmail.com>
2025-02-21 12:10:30 -0300

    feat(mfe-login): add login page

--- projects/mfe-login/src/app/app.component.ts
+++ projects/mfe-login/src/app/app.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/native-federation';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  DoBootstrap,
+  inject,
+  Injector,
+  OnInit
+} from '@angular/core';
+
+import { createCustomElement } from '@angular/elements';
 
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppComponent {
-  title = 'mfe-login';
+export class AppComponent implements OnInit {
+  private injector = inject(Injector);
+
+  async ngOnInit() {
+    const elements: any[] = [
+      ['ButtonComponent', 'ds-button'],
+      ['FieldComponent', 'ds-field'],
+    ];
+
+    for (let [name, tag] of elements) {
+      const m = await loadRemoteModule({
+        remoteEntry: 'http://localhost:4201/remoteEntry.json',
+        exposedModule: `./${name}`,
+      });
+
+      const element = createCustomElement(m[name], {
+        injector: this.injector,
+      });
+
+      customElements.define(tag, element);
+    }
+  }
 }
--- projects/mfe-login/src/main.ts
+++ projects/mfe-login/src/main.ts
@@ -1,6 +1,30 @@
-import { initFederation } from '@angular-architects/native-federation';
 
-initFederation()
-  .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
-  .catch(err => console.error(err));
+import {
+  initFederation,
+  loadRemoteModule,
+} from '@angular-architects/native-federation';
+import { createCustomElement } from '@angular/elements';
+import { createApplication } from '@angular/platform-browser';
+
+initFederation('federation.manifest.json')
+  .catch((err) => console.error(err))
+  .then((_) => import('./bootstrap'))
+  .catch((err) => console.error(err));
+
+  // (async () => {
+  //   const app = await createApplication({
+  //     providers: [
+  //       /* your global providers here */
+  //     ],
+  //   });
+
+  //   const m = await loadRemoteModule({
+  //     remoteEntry: 'http://localhost:4201/remoteEntry.json',
+  //     exposedModule: './ButtonComponent',
+  //   });
+  //   const buttonElement = createCustomElement(m.ButtonComponent, {
+  //     injector: app.injector,
+  //   });
+
+  //   customElements.define('ds-button', buttonElement);
+  // })();
