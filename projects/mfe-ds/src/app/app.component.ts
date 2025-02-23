43b2c78d89fac3c8a8c3bdce884aeb5435470469
Wiliane Souza <wiliane_souza@hotmail.com>
2025-02-23 09:08:07 -0300

    style(mfe-ds): fix nav bar responsiviness

--- projects/mfe-ds/src/app/app.component.ts
+++ projects/mfe-ds/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
-import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    SideBarComponent
+    NavBarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
